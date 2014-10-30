e0d931410238d4ce1851366c3d1c4e0a9587ee4c
sindrikaldal <sindrimars@gmail.com>
2014-10-30 14:17:50 +0000

    modified PlayerTest and Player

---
+++ src/test/java/ttt/PlayerTest.java
@@ -0,0 +1,19 @@
+package ttt;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class PlayerTest {
+
+	public static void main(String args[]) {
+		org.junit.runner.JUnitcore.main("ttt.PlayerTest");
+	}
+
+	@Test
+	public void testGetName() {
+		assertEquals("Sindri", Player.getName)
+	}
+
+
+
+}
