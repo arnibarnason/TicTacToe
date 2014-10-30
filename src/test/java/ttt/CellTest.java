763b4a2673b9c52f542c1558ccbb9119eac7cc90
Daniel Benediktsson <danielbenediktsson@Daniels-MacBook-Air-2.local>
2014-10-30 15:15:13 +0000

    Created a contructor for Cell and added a test for function isChecked

--- src/test/java/ttt/CellTest.java
+++ src/test/java/ttt/CellTest.java
@@ -1,5 +1,19 @@
 package ttt;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 public class CellTest {
-	
+
+	public static void main(String args[]) {
+		org.junit.runner.JUnitCore.main("ttt.CellTest");
+	}
+
+	@Test
+	public void isCheckedTest() {
+		Cell cell = new Cell(0);
+		assertEquals(false, cell.isChecked());
+	}
+
+
 }
