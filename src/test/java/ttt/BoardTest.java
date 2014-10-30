f5e5855f9bdbc5eacd1ef295a0758aa186a42d8a
ArniB <arnibarnason@gmail.com>
2014-10-30 16:31:54 +0000

    added function markCell with base test

--- src/test/java/ttt/BoardTest.java
+++ src/test/java/ttt/BoardTest.java
@@ -1,12 +1,18 @@
 package ttt;
 
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
 public class BoardTest {
+
 		public static void main(String[] args) {
 			org.junit.runner.JUnitCore.main("ttt.BoardTest");
 		}
 
-/*		@Test
-        public void () {
-            assertEquals(0, Board.markCell());
-        }*/
+		@Test
+        public void testSignIsCorrect () {
+        	Board board = new Board();
+        	board.markCell(3, 'X');
+            assertEquals('X', board.getBoard()[1][0].getSign());
+        }
 }
