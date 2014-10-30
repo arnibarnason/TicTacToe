b028192a165c762f6a5e1b6c199577a9bee5cd08
sindrikaldal <sindrimars@gmail.com>
2014-10-30 20:09:22 +0000

    Added a play function to TicTacToe

--- src/main/java/ttt/TicTacToe.java
+++ src/main/java/ttt/TicTacToe.java
@@ -1,16 +1,19 @@
 package ttt;
+import java.util.Scanner;
 
 public class TicTacToe {
 	private Board _board;
 	private Player _player1;
 	private Player _player2;
 	private int _round;
+	private int _ties;
 	
 	public TicTacToe() {
 		_board = new Board();
 		_player1 = new Player("Player 1", 'X');		
 		_player2 = new Player("Player 2", 'O');
 		_round = 0;
+		_ties = 0;
 	}	
 
 	public String greeting() {
@@ -19,6 +22,10 @@ public class TicTacToe {
 
 	public void increaseRound() {
 		_round++;
+	}
+
+	public void increaseTies() {
+		_ties++;
 	}
 
 	public int getRound() {
@@ -34,6 +41,7 @@ public class TicTacToe {
 	}
 	
 	public Player switchPlayer(){
+
 		if(_round % 2 == 0) {
 			return getPlayer1();
 		}
@@ -42,11 +50,35 @@ public class TicTacToe {
 		}
 	} 
 
-
 	public boolean isOver() {
 		return _board.isFull() || _board.isWinner();
 
 	}
+
+	public void play(int number) {
+
+		Player currPlayer = _player1;
+
+		while(!isOver()) {
+
+			//_board.displayBoard();
+
+			currPlayer = switchPlayer();
+
+			_board.markCell(number, currPlayer.getSign());
+
+			increaseRound();
+		}
+
+		if(_board.isWinner()) {
+			currPlayer.increaseScore();
+		}
+
+		if(_board.isFull() && !_board.isWinner()) {
+			increaseTies();
+		}
+	}
+
 
 
 
