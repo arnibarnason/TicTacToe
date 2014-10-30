adf964bb34723f7dbc7f5ca1cd982b00f6f86853
Magnús Sigurðarson <maggisig92@gmail.com>
2014-10-30 17:17:32 +0000

    fixed encoding in TTTTest

--- src/test/java/ttt/TicTacToeTest.java
+++ src/test/java/ttt/TicTacToeTest.java
@@ -16,7 +16,7 @@ public class TicTacToeTest {
 	public void testGetPlayer() {
 		assertEquals("Player 1", ttt.getPlayer1().getName());
 	}
-	//Magnús og Sindri vinna hér
+	//Magnus og Sindri vinna her
 	@Test
 	public void testSwitchPlayer() {
 		assertEquals("Player 1", ttt.switchPlayer().getName());
