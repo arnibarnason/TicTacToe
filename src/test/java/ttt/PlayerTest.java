92b25194777711b1f0482e80630fc2351c6ff7f7
danben4 <danielbenediktsson4@gmail.com>
2014-10-31 11:00:54 +0000

    Fixed player tests

--- src/test/java/ttt/PlayerTest.java
+++ src/test/java/ttt/PlayerTest.java
@@ -5,39 +5,39 @@ import org.junit.Test;
 
 public class PlayerTest {
 
-	/*public static void main(String args[]) {
-		org.junit.runner.JUnitcore.main("ttt.PlayerTest");
-	}*/
-
-	Player p = new Player("Sindri", 'X');
-
+	public static void main(String args[]) {
+		org.junit.runner.JUnitCore.main("ttt.PlayerTest");
+	}
 
 	@Test
 	public void testGetName() {
+		Player p = new Player("Sindri", 'X');
 		assertEquals("Sindri", p.getName());
 	}
 
 	@Test
 	public void testGetSign() {
+		Player p = new Player("Kalli", 'X');
 		assertEquals('X', p.getSign());
 	}
 
 	@Test
 	public void testSetName() {
+		Player p = new Player("Vignir", 'X');
 		p.setName("Magnus");
 		assertEquals("Magnus", p.getName());
 	}
 	
 	@Test
 	public void testGetScore() {
+		Player p = new Player("Daniel", 'X');
 		assertEquals(0, p.getScore());
 	}
 
 	@Test
 	public void testIncreaseScore() {
+		Player p = new Player("Sindri", 'X');
 		p.increaseScore();
 		assertEquals(1, p.getScore());
 	}
-
-
 }
