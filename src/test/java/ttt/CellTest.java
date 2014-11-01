1e6da46697ae14020ad032cd7cfff33bfa731505
hilmarragnars <hilmarragnars@gmail.com>
2014-11-01 12:23:04 +0000

    Finnished fixing rule vioalations in cellTest.java

--- src/test/java/ttt/CellTest.java
+++ src/test/java/ttt/CellTest.java
@@ -1,55 +1,57 @@
 package ttt;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 import org.junit.Test;
 
 public class CellTest {
 
-	public static void main(String args[]) {
+	public static void main(final String args[]) {
 		org.junit.runner.JUnitCore.main("ttt.CellTest");
 	}
 
 	@Test
 	public void isCheckedTest() {
-		Cell cell = new Cell(0);
+		final Cell cell = new Cell(0);
 		assertFalse("Returned true, sould be false.", cell.isChecked());
 	}
 
 	@Test
 	public void setCheckedTest() {
-		Cell cell = new Cell(0);
+		final Cell cell = new Cell(0);
 		cell.setChecked();
 		assertTrue("Returned false, sould be true.", cell.isChecked());
 	}
 
 	@Test
 	public void getSignTest() {
-		Cell cell = new Cell(0);
+		final Cell cell = new Cell(0);
 		assertEquals("The sign was not a whiteline!", ' ', cell.getSign());
 	}
 
 	@Test
 	public void setSignTest() {
-		Cell cell = new Cell(0);
+		final Cell cell = new Cell(0);
 		cell.setSign('X');
 		assertEquals("The sign was not X.", 'X', cell.getSign());
 	}
 
 	@Test
 	public void getNumberTest() {
-		Cell cell = new Cell(5);
+		final Cell cell = new Cell(5);
 		assertEquals("Did not get the right number, 5.", 5, cell.getNumber());
 	}
 
 	@Test
 	public void getPartOfWinningLineTest() {
-		Cell cell = new Cell(0);
+		final Cell cell = new Cell(0);
 		assertFalse("Returned true, sould return false" ,cell.getPartOfWinningLine());
 	}
 
 	@Test
 	public void setPartOfWinnigLineTest() {
-		Cell cell = new Cell(0);
+		final Cell cell = new Cell(0);
 		cell.setPartOfWinningLine();
 		assertTrue("Returned false, should be true." ,cell.getPartOfWinningLine());
 	}
