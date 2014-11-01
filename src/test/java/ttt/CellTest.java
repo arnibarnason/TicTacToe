30364003334da734736a960be3da709d84f608b9
hilmarragnars <hilmarragnars@gmail.com>
2014-11-01 12:15:37 +0000

    Fixed some rule violations in CallTest.java

--- src/test/java/ttt/CellTest.java
+++ src/test/java/ttt/CellTest.java
@@ -12,45 +12,45 @@ public class CellTest {
 	@Test
 	public void isCheckedTest() {
 		Cell cell = new Cell(0);
-		assertEquals(false, cell.isChecked());
+		assertFalse("Returned true, sould be false.", cell.isChecked());
 	}
 
 	@Test
 	public void setCheckedTest() {
 		Cell cell = new Cell(0);
 		cell.setChecked();
-		assertEquals(true, cell.isChecked());
+		assertTrue("Returned false, sould be true.", cell.isChecked());
 	}
 
 	@Test
 	public void getSignTest() {
 		Cell cell = new Cell(0);
-		assertEquals(' ', cell.getSign());
+		assertEquals("The sign was not a whiteline!", ' ', cell.getSign());
 	}
 
 	@Test
 	public void setSignTest() {
 		Cell cell = new Cell(0);
 		cell.setSign('X');
-		assertEquals('X', cell.getSign());
+		assertEquals("The sign was not X.", 'X', cell.getSign());
 	}
 
 	@Test
 	public void getNumberTest() {
 		Cell cell = new Cell(5);
-		assertEquals(5, cell.getNumber());
+		assertEquals("Did not get the right number, 5.", 5, cell.getNumber());
 	}
 
 	@Test
 	public void getPartOfWinningLineTest() {
 		Cell cell = new Cell(0);
-		assertEquals(false, cell.getPartOfWinningLine());
+		assertFalse("Returned true, sould return false" ,cell.getPartOfWinningLine());
 	}
 
 	@Test
 	public void setPartOfWinnigLineTest() {
 		Cell cell = new Cell(0);
 		cell.setPartOfWinningLine();
-		assertEquals(true, cell.getPartOfWinningLine());
+		assertTrue("Returned false, should be true." ,cell.getPartOfWinningLine());
 	}
 }
